9359d7ff029fc2e2762b95616593eb704cedc33b
creation <creation2224@gmail.com>
2025-06-01 12:58:45 +0530

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Frontpage from './components/Frontpage'
 import OutputPage from './components/OutputPage'
-
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <Router>
   
       <Routes>
+        <Analytics/>
         <Route path="/" element={<Frontpage />} />
         <Route path="/output" element={<OutputPage />} />
       </Routes>
