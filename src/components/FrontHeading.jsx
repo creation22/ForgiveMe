31a4e7304924ace97ff550724c34643718091125
creation <creation2224@gmail.com>
2025-06-01 13:37:57 +0530

    fool helped

--- src/components/FrontHeading.jsx
+++ src/components/FrontHeading.jsx
@@ -1,26 +1,29 @@
 "use client";
 import { ContainerTextFlip } from "./ui/container-text-flip";
-import { motion } from "motion/react";
+import { motion } from "framer-motion"; // corrected import
 import { cn } from "../lib/utilss";
 
 export function ContainerTextFlipDemo() {
-  const words = ["ForgiveMe", " माफ़ करना", "ক্ষমা করো", "மன்னிக்கவும்"];
-  return (
-    <motion.h1
-      initial={{
-        opacity: 0,
-      }}
-      whileInView={{
-        opacity: 1,
-      }}
-      className={cn(
-        "relative mb-6  text-left text-4xl leading-normal font-bold tracking-tight text-zinc-700 md:text-7xl dark:text-zinc-100"
-      )}
-      layout>
-      <div className="inline-block">
-        Bridge the gap with Words that Heals <ContainerTextFlip words={words} />
-        {/* <Blips /> */}
-      </div>
-    </motion.h1>
-  );
+const words = ["ForgiveMe", " माफ़ करना", "ক্ষমা করো", "மன்னிக்கவும்"];
+
+return (
+<div className="flex justify-center">
+<motion.h1
+initial={{ opacity: 0 }}
+whileInView={{ opacity: 1 }}
+className={cn(
+"relative mb-6 text-center text-4xl leading-normal font-bold tracking-tight text-zinc-700 md:text-7xl dark:text-zinc-100"
+)}
+layout
+>
+<div className="inline-block text-5xl">
+Bridge the gap with Words that Heals{" "}
+<span className="flex justify-center mt-3">
+{" "}
+<ContainerTextFlip words={words} />
+</span>
+</div>
+</motion.h1>
+</div>
+);
 }
